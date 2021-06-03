828e2f99108222ab96b0b2bf27b14a936d44397a
Nick Kuipers <nickkuipers92@gmail.com>
2021-06-03 14:46:28 +0200

    first push

---
+++ philo_one/philo_one.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   philo_one.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nkuipers <nkuipers@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/06/03 14:03:25 by nkuipers      #+#    #+#                 */
+/*   Updated: 2021/06/03 14:24:07 by nkuipers      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_ONE_H
+# define PHILO_ONE_H
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/wait.h>
+# include <sys/time.h>
+
+int				main(int ac, char **av);
+
+#endif
